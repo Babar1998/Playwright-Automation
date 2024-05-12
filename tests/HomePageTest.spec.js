89c99c522bb98ced1297dfdeb43715482479a443
Babar Arif <babararifmasood98@gmail.com>
2024-05-12 23:43:04 +0500

    Playwright Test Cases

---
+++ tests/HomePageTest.spec.js
@@ -0,0 +1,12 @@
+const {test, expect} = require('@playwright/test')
+
+test('Home Page', async ({page}) => {
+    await page.goto('https://demoblaze.com/index.html')
+    const pageTitle = await page.title();
+    console.log("Page Title is: " + pageTitle);
+    await expect(page).toHaveTitle("STORE");
+    const pageUrl = page.url();
+    console.log("Page URL is" + pageUrl);
+    await expect(page).toHaveURL("https://demoblaze.com/index.html")
+    await page.close(); 
+}) 
